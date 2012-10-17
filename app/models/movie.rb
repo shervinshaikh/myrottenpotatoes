a03eb953b6e8320fe60aee1fd3e278ddc933cac5
Shervin Shaikh <shervinater@gmail.com>
2012-10-17 00:58:20 -0700

    Re-order the checkboxes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,11 @@
 class Movie < ActiveRecord::Base
   def self.getratings
-    movie = self.find(:all, :select => 'DISTINCT rating')
-    array = []
-    movie.each do |t|
-      array << t.rating
-    end
-    return array
+#    movie = self.find(:all, :select => 'DISTINCT rating')
+#    array = []
+#    movie.each do |t|
+#      array << t.rating
+#    end
+#    return array
+    return ['G', 'PG', 'PG-13', 'R']
   end
 end
