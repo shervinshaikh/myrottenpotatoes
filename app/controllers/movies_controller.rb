9056d5bdc0dba67f342f4aa711614a8599a9a01a
Shervin Shaikh <shervinater@gmail.com>
2012-10-21 19:15:15 -0700

    New code testing out on herokue

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,31 +1,69 @@
 class MoviesController < ApplicationController
   
   def index
-#@movies = Movie.all
-#@movies = Movie.find(:all, :order => params[:sort])
     @title_header, @release_header = nil
+    @allratings = Movie.getratings
+    @selected_ratings = @allratings
     if params[:sort] == "title ASC"
       @title_header = "hilite"
     elsif params[:sort] == "release_date ASC"
       @release_date_header = "hilite"
     end
-    @allratings = Movie.getratings
-    @selected_ratings = @allratings
 
+#flash[:ratings] = params[:ratings].keys if params[:ratings] != nil
+##session[:sort] = params[:sort] if params[:sort] != nil || params[:sort] != ""
+#@selected_ratings = flash[:ratings] if flash[:ratings] != nil
 
-    flash[:ratings] = params[:ratings].keys if params[:ratings] != nil
-#session[:sort] = params[:sort] if params[:sort] != nil || params[:sort] != ""
-    @selected_ratings = flash[:ratings] if flash[:ratings] != nil
-    @movies = Movie.find_all_by_rating(@selected_ratings, :order => params[:sort])
+    if params[:commit]
+      session.delete(:ratings)
+      if session[:sort]
+        sort = session[:sort]
+        session.delete(:sort)
+        redirect_to(:action => "index", :sort => sort)
+      else
+        redirect_to(:action => "index")
+      end
+    end
 
-#    if params[:ratings] != nil
-#      @movies = Movie.find_all_by_rating(params[:ratings].keys, :order => params[:sort])
-#      session[:ratings] = params[:ratings].keys
-#      @selected_ratings = session[:ratings]
-#      session[:sort] = params[:sort]
-#    elsif
-#      @movies = Movie.find_all_by_rating(session[:ratings], :order => session[:sort])
-#    end
+    if params[:sort]
+      sort = params[:sort]
+    elsif session[:sort]
+      sort = session[:sort]
+      session.delete(:sort)
+      if session[:ratings]
+        @ratings = session[:ratings]
+        session.delete(:ratings)
+        redirect_to(:action => "index", :sort => sort, :ratings => @ratings)
+      else
+        redirect_to(:action => "index", :sort => sort)
+      end
+    end
+
+    if params[:ratings]
+      @ratings = params[:ratings]
+    elsif session[:ratings]
+      @ratings = session[:ratings]
+      session.delete(:ratings)
+      if session[:sort]
+        sort = session[:sort]
+        session.delete(:sort)
+        redirect_to(:action => "index", :sort => sort, :ratings => @ratings)
+      else
+        redirect_to(:action => "index", :ratings => @ratings)
+      end
+    end
+
+    @selected_ratings = @ratings.keys if @ratings
+
+    if sort
+      @movies = Movie.find_all_by_rating(@selected_ratings, :order => sort)
+    elsif
+      @movies = Movie.find_all_by_rating(@selected_ratings)
+    end
+    
+    session[:sort] = sort if params[:sort]
+    session[:ratings] = @ratings if params[:ratings]
+
   end
 
   def show
@@ -33,10 +71,6 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-
-#def index
-#    @movies = Movie.all
-#  end
 
   def new
     # default: render 'new' template
